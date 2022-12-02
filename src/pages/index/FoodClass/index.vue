947ab409ba4fcaf31fbb05823b2840a0ceec42f2
coldqiu <gotta_blue@163.com>
2022-12-02 17:28:58 +0800

    首页，增加动画

--- src/pages/index/FoodClass/index.vue
+++ src/pages/index/FoodClass/index.vue
@@ -1,11 +1,12 @@
 <template>
   <view class="food_class">
     <!-- <text>page3 HRM</text> -->
-    <view class="wrap" v-for="itemA in list" :key="itemA.type">
+    <view class="wrap" v-for="itemA in foodList" :key="itemA.type">
       <view class="title">{{ itemA.type }}</view>
       <view class="list">
-        <view class="item" v-for="itemB in itemA.list" :key="itemB">
-          {{ itemB }}
+        <view :class="['item', itemB.checked === true ? 'checked' : '']" @tap="handleClick($event, itemB)"
+          v-for="itemB in itemA.list" :key="itemB.name">
+          {{ itemB.name }}
         </view>
       </view>
     </view>
@@ -14,6 +15,8 @@
 </template>
 
 <script>
+import { reactive, ref, defineEmits } from 'vue'
+import { vibrateShort } from '@tarojs/taro'
 import './index.scss'
 
 export default {
@@ -21,9 +24,24 @@ export default {
 }
 </script>
 <script setup>
-defineProps({
+const props = defineProps({
   list: {
     type: Array,
   }
 })
+const foodList = reactive(props.list)
+
+const checkedCount = ref(0)
+const emit = defineEmits(['change'])
+function handleClick(e, food) {
+  food.checked = !food.checked
+  if (food.checked) {
+    checkedCount.value = checkedCount.value + 1
+  } else {
+    checkedCount.value = checkedCount.value - 1;
+  }
+  emit('change', checkedCount.value)
+  vibrateShort({ type: 'light' })
+}
+
 </script>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -3,7 +3,7 @@
     <CompNavigationBar :position="'sticky'">
       <view class="flex">
         Home
-        <view class="search">搜索</view>
+        <view :class="['search', scrollTop > 100 ? 'show' : '']">搜索</view>
       </view>
     </CompNavigationBar>
 
@@ -21,7 +21,12 @@
     </view>
     <view class="hi">选食材</view>
     <view class="food_class_wrap">
-      <FoodClass :list="foodClass" />
+      <FoodClass :list="foodClass" @change="handleChange" />
+    </view>
+    <view :class="['search_wrap', checkedCount > 0 ? 'show' : '']">
+      <view class="text">
+        搜索
+      </view>
     </view>
   </view>
 </template>
@@ -33,27 +38,46 @@ export default {
 }
 </script>
 <script setup>
-import { useReady } from '@tarojs/taro'
+import { ref } from 'vue'
+import { useReady, usePageScroll } from '@tarojs/taro'
 import CompNavigationBar from '../../components/CompNavigationBar/index.vue';
 import FoodClass from './FoodClass/index.vue'
 
 useReady(() => {
   // console.log('useReady: ', useReady);
 })
+// 造数据
 const temp = 'abcdefghijklmnopqrstuvwxyz'
 const tempLower = new Array(12).fill(0).map(() => {
   let index = Math.ceil(Math.random() * 16)
   let length = Math.ceil(4 + Math.random() * 6)
-  return temp.slice(index, index + length)
+  return {
+    checked: false, name: temp.slice(index, index + length)
+  }
 })
-console.log('tempLower: ', tempLower);
 const tempUpper = new Array(12).fill(0).map(() => {
   let index = Math.ceil(Math.random() * 16)
   let length = Math.ceil(4 + Math.random() * 6)
-  return temp.slice(index, index + length).toUpperCase()
+  return {
+    checked: false, name: temp.slice(index, index + length)
+  }
 })
 const foodClass = [
   { type: '蔬菜', list: tempLower },
   { type: '肉类', list: tempUpper }
 ]
+// 选中食材
+const checkedCount = ref(0)
+function handleChange(count) {
+  console.log('count: ', count);
+  checkedCount.value = count
+}
+// usePageScroll 顶部搜索图标 动画
+const scrollTop = ref(0)
+usePageScroll(res => {
+  console.log('res: ', res);
+  scrollTop.value = res.scrollTop
+  // if (res.scrollTop > 100) 
+})
+// 点击搜索框 重置选中食材
 </script>
