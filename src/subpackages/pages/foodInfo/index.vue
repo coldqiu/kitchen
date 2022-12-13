b3848fcdb5c7a54c7da1796fdfe884e7383828eb
coldqiu <gotta_blue@163.com>
2022-12-13 11:12:54 +0800

    初始化 foodInfo

--- src/subpackages/pages/foodInfo/index.vue
+++ src/subpackages/pages/foodInfo/index.vue
@@ -1,6 +1,63 @@
 <template>
   <view class="page">
-    <InfoNavigationBar title="玲龙一号”核反应堆，在海南正式开工" />
+    <InfoNavigationBar :title="params.id" />
+    <image src="https://cdn.coldqiu.com/136145419569074941.jpg"></image>
+    <view class="food_title">
+      <view class="title">{{ params.id }}</view>
+      <view class="desc_wrap">
+        <text class="rating">8.0综合评分</text>
+        <text class="dot">·</text>
+        <text class="done_count">16人最近7天做过</text>
+      </view>
+    </view>
+    <view class="member_wrap">
+      <view class="list">
+        <view class="item">
+          <view class="content">
+            <view class="img">
+              <image src=""></image>
+            </view>
+            <view class="name">
+              I am Iron Man.
+            </view>
+          </view>
+          <view class="follow">follow</view>
+        </view>
+        <view class="desc">
+          描述一词的说明：描述某人/某事物的文字或演讲；用文字写出或说出某人/某物的样子的行为
+        </view>
+      </view>
+    </view>
+    <view class="materials_wrap">
+      <view class="title">
+        <view class="text">materials</view>
+        <view class="button">add button</view>
+      </view>
+      <view class="list">
+        <view v-for="item in tmpList" :key="item.name" class="item">
+          <view class="name">{{ item.name }}</view>
+          <view class="num">{{ item.name }}</view>
+        </view>
+      </view>
+    </view>
+    <view class="step_wrap">
+      <view class="list">
+        <view v-for="(item, index) in tmpList" :key="item.name" class="item">
+          <view class="title">step {{ index + 1 }}</view>
+          <!-- <image width="100%" height="100vw" src="https://cdn.coldqiu.com/69eea64c6fdd43bd882f64bb016f1275.jpeg" /> -->
+          <!-- background-image: url("https://cdn.coldqiu.com/69eea64c6fdd43bd882f64bb016f1275.jpeg"); -->
+          <view class="img">
+
+          </view>
+          <view class="desc">{{ randomText(0, 25) }}</view>
+        </view>
+      </view>
+      <view class="tips">
+        <view class="title"> 小贴士</view>
+        如果毛衣袖口或下摆失去了弹性，可在热水中浸泡20分钟，通风干燥处晾干后即可恢复弹性。
+        <view class="content"></view>
+      </view>
+    </view>
   </view>
 </template>
 
@@ -12,11 +69,13 @@ export default {
 <script setup>
 import { useRouter } from '@tarojs/taro'
 import InfoNavigationBar from '@/components/InfoNavigationBar/index.vue'
+import { tempUpper, randomText } from '@/utils/data.js'
 import './index.scss'
 
-const router = useRouter()
-console.log('router: ', router);
+const { params } = useRouter()
+// console.log('router: ', router);
+const tmpList = tempUpper.slice(0, 3)
 </script>
-<style lang='scss' scoped>
+<style lang='scss'>
 
 </style>
